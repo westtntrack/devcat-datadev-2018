7caa9f13567d1731d37deddc2d1c09a2834126e9
Michael Plyler <mplyler@fhu.edu>
2018-07-20 09:01:59 -0500

    slight update to sql script

--- sample-database/northwind.sql
+++ sample-database/northwind.sql
@@ -4,9 +4,9 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-DROP SCHEMA IF EXISTS `northwind` ;
-CREATE SCHEMA IF NOT EXISTS `northwind` DEFAULT CHARACTER SET latin1 ;
-USE `northwind` ;
+-- DROP SCHEMA IF EXISTS `northwind` ;
+-- CREATE SCHEMA IF NOT EXISTS `northwind` DEFAULT CHARACTER SET latin1 ;
+-- USE `northwind` ;
 
 -- -----------------------------------------------------
 -- Table `northwind`.`customers`
